a7ea3d9abd8198ec92083a2ed6810f0a54892b0b
Randy Jones <randysjones@gmail.com>
2018-03-07 00:57:39 -0500

    Adding widget created from 'yo esri-appbuilder-js:widget'

---
+++ widgets/MyWidget/nls/strings.js
@@ -0,0 +1,9 @@
+define({
+  root: {
+    _widgetLabel: 'My Widget',
+    widgetTitle: 'My Widget',
+    description: 'A custom Web AppBuilder widget.'
+  }
+  // add supported locales below:
+  // , "zh-cn": true
+});
